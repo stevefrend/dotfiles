f8ab14d7ee19e2af639e980471e284edbc6f91af
sfrendpax8 <sfrend@pax8.com>
2024-09-27 08:43:11 -0600

    nvim: lazygit integration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -242,6 +242,21 @@ require('lazy').setup({
     end,
   },
   {
+    'kdheepak/lazygit.nvim',
+    event = 'VeryLazy',
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    opts = {},
+    config = function()
+      local g = require 'lazygit'
+      vim.keymap.set('n', '<leader>g', function()
+        g.lazygit()
+      end, { desc = 'Git' })
+    end,
+  },
+  {
     -- Emmet type auto-closing tags for html etc
     'windwp/nvim-ts-autotag',
     opts = {},
