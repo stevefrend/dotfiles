fcecbf9d8f95b5b1ff507a46c5731deb2f958652
sfrendpax8 <sfrend@pax8.com>
2024-10-03 17:15:22 -0600

    nvim: git blame

---
+++ .config/nvim/lua/custom/plugins/git-blame.lua
@@ -0,0 +1,15 @@
+return {
+  'f-person/git-blame.nvim',
+  event = 'VimEnter',
+  opts = {
+    enabled = true,
+    message_template = ' <summary> • <date> • <author> • <<sha>>',
+    date_format = '%m-%d-%Y %H:%M:%S',
+    virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+  },
+  config = function()
+    vim.cmd [[GitBlameToggle]] -- default to toggling it off
+    vim.keymap.set('n', '<leader>gbo', '<CMD>GitBlameOpenCommitURL<CR>', { desc = '[G]it [B]lame [O]pen' })
+    vim.keymap.set('n', '<leader>gb', '<CMD>GitBlameToggle<CR>', { desc = '[Git] [B]lame Toggle' })
+  end,
+}
