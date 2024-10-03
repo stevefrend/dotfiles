ac4829e759d4a37e16ed62f799cba24fa1217612
sfrendpax8 <sfrend@pax8.com>
2024-10-03 16:35:54 -0600

    nvim: telescope undo tree keymap

--- .config/nvim/lua/custom/plugins/telescope.lua
+++ .config/nvim/lua/custom/plugins/telescope.lua
@@ -79,6 +79,9 @@ return { -- Fuzzy Finder (files, lsp, etc)
     pcall(require('telescope').load_extension, 'frecency')
     pcall(require('telescope').load_extension, 'undo')
 
+    -- Extension Keymaps
+    vim.keymap.set('n', '<leader>su', '<CMD>Telescope undo<CR>', { desc = '[ ] Find existing buffers' })
+
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
@@ -86,10 +89,8 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sf', builtin.git_files, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>sn', builtin.find_files, { desc = '[S]earch [N]ormal' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-    -- vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
