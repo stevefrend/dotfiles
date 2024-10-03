e093b6339497e8693f01a071ec6b6be0daa0960c
sfrendpax8 <sfrend@pax8.com>
2024-10-03 09:35:54 -0600

    nvim: remove some lualine noise and update transparency of alacritty

--- .config/nvim/lua/custom/plugins/lualine.lua
+++ .config/nvim/lua/custom/plugins/lualine.lua
@@ -1,5 +1,14 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {},
+  opts = {
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diagnostics' },
+      lualine_c = { 'filename' },
+      lualine_x = { 'filetype' },
+      lualine_y = {},
+      lualine_z = {},
+    },
+  },
 }
