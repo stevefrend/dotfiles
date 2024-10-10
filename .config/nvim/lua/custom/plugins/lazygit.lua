474da443d017302038cff3465bf48732940d7867
sfrendpax8 <sfrend@pax8.com>
2024-10-10 10:47:41 -0600

    nvim: lazygit config file and enable esc to close

--- .config/nvim/lua/custom/plugins/lazygit.lua
+++ .config/nvim/lua/custom/plugins/lazygit.lua
@@ -1,13 +1,19 @@
 return {
   'kdheepak/lazygit.nvim',
-  event = 'VeryLazy',
+  event = 'VimEnter',
   -- optional for floating window border decoration
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
-  opts = {},
   config = function()
+    local home = os.getenv 'HOME'
+    local correct_path = home .. '/.config/lazygit/config.yml'
+
+    vim.g.lazygit_use_custom_config_file_path = 1
+    vim.g.lazygit_config_file_path = correct_path
+
     local g = require 'lazygit'
+
     vim.keymap.set('n', '<leader>g', function()
       g.lazygit()
     end, { desc = 'Git' })
