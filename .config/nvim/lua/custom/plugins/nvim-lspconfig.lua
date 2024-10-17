83a217b9a0906c8d6b870bdec672403382eaba62
steve <stevefrend1@gmail.com>
2024-10-17 06:26:11 -0600

    nvim: typescript-tools is not a language server

--- .config/nvim/lua/custom/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/custom/plugins/nvim-lspconfig.lua
@@ -191,7 +191,6 @@ return {
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
-      'typescript-tools',
       'prettier',
       'eslint',
       'stylua', -- Used to format Lua code
