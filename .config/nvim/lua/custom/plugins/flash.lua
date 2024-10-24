fd09c742be7558a1fd94b9d0408c4ec14b3bdd69
sfrendpax8 <sfrend@pax8.com>
2024-10-23 20:58:18 -0600

    nvim: flash config, use with normal search

--- .config/nvim/lua/custom/plugins/flash.lua
+++ .config/nvim/lua/custom/plugins/flash.lua
@@ -2,15 +2,11 @@ return {
   'folke/flash.nvim',
   event = 'VeryLazy',
   ---@type Flash.Config
-  opts = {},
-  keys = {
-    {
-      's',
-      mode = { 'n' },
-      function()
-        require('flash').jump()
-      end,
-      desc = 'Flash',
+  opts = {
+    modes = {
+      search = {
+        enabled = true,
+      },
     },
   },
 }
