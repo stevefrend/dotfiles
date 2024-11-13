f5578e3aa5454eadb38540a26c3aef2560121b02
sfrendpax8 <sfrend@pax8.com>
2024-11-13 14:52:20 -0700

    nvim: copilot updates (work only)

---
+++ .config/nvim/lua/custom/plugins/copilot-chat.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    enabled = vim.env.LOCATION == 'work',
+    branch = 'canary',
+    dependencies = {
+      { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      debug = false,
+      model = 'claude-3.5-sonnet',
+      window = {
+        width = 0.45,
+      },
+      auto_insert_mode = true,
+      insert_at_end = true,
+    },
+    keys = {
+      {
+        '<leader>ch',
+        ':CopilotChatToggle<CR>',
+        mode = { 'n' },
+        desc = 'CopilotChat - Toggle',
+      },
+      {
+        '<leader>cc',
+        function()
+          local input = vim.fn.input 'Quick Chat: '
+          if input ~= '' then
+            require('CopilotChat').ask(input, { selection = require('CopilotChat.select').visual })
+          end
+        end,
+        mode = { 'v' },
+        desc = 'CopilotChat - Ask with Selection',
+      },
+    },
+  },
+}
--- .config/nvim/lua/custom/plugins/copilot.lua
+++ .config/nvim/lua/custom/plugins/copilot.lua
@@ -1,24 +1,28 @@
+-- return {
+--   'zbirenbaum/copilot.lua',
+--   cmd = 'Copilot',
+--   event = 'InsertEnter',
+--   opt = {},
+--   enabled = vim.env.LOCATION == 'work',
+--   config = function()
+--     require('copilot').setup {
+--       panel = {
+--         enabled = false,
+--       },
+--       suggestion = {
+--         auto_trigger = true,
+--         debounce = 60,
+--         keymap = {
+--           -- prev = '<leader>j',
+--           -- next = '<leader>k',
+--           accept = '<Tab>',
+--           -- dismiss = '<leader>ll',
+--         },
+--       },
+--     }
+--   end,
+-- }
 return {
-  'zbirenbaum/copilot.lua',
-  cmd = 'Copilot',
-  event = 'InsertEnter',
-  opt = {},
+  'github/copilot.vim',
   enabled = vim.env.LOCATION == 'work',
-  config = function()
-    require('copilot').setup {
-      panel = {
-        enabled = false,
-      },
-      suggestion = {
-        auto_trigger = true,
-        debounce = 60,
-        keymap = {
-          -- prev = '<leader>j',
-          -- next = '<leader>k',
-          accept = '<Tab>',
-          -- dismiss = '<leader>ll',
-        },
-      },
-    }
-  end,
 }
