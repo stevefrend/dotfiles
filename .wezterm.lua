62101173fdf77055f7051564f030f3a4ae077823
steve <stevefrend1@gmail.com>
2026-02-27 13:36:21 -0700

    terminal: enable wezterm top bar

--- .wezterm.lua
+++ .wezterm.lua
@@ -19,9 +19,9 @@ config.window_padding = {
 	top = 20,
 	bottom = 20,
 }
-config.window_decorations = "RESIZE" -- Buttonless decoration
+-- config.window_decorations = "RESIZE" -- Buttonless decoration
 config.window_background_opacity = 0.8
-config.enable_tab_bar = false
+-- config.enable_tab_bar = false
 config.macos_window_background_blur = 45 -- Blur effect for macOS
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
