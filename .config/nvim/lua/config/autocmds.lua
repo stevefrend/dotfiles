6535c816dd45bb027ccb40e6007fb7708a2f14be
sfrendpax8 <sfrend@pax8.com>
2026-08-12 11:34:42 -0600

    nvim: do not format yml
    mostly temporarily, don't want my prs to include yaml formats all the
    time

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable format-on-save for filetypes without an enforced/required formatter
+-- config in the project (unlike TS projects, which require a prettier config).
+-- Formatting is still available on-demand via <leader>cf / :LazyFormat.
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("disable_autoformat", { clear = true }),
+  pattern = { "kotlin", "yaml", "yaml.docker-compose" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
